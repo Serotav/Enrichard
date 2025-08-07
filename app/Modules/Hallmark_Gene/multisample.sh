ba51a93266b367437591bff00b7c02a10f19a83f
Serotav <salvo.gulizia13@gmail.com>
2025-08-07 14:06:57 +0200

    fisher and ttest on multiple samples, missing graphs now

---
+++ app/Modules/Hallmark_Gene/multisample.sh
@@ -0,0 +1,6 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+PYTHON_GROUP_SCRIPT="$SCRIPT_DIR/analyze_groups.py"
+USER_DIR=$1
+METHOD=$2
+
+python3 $PYTHON_GROUP_SCRIPT  --analysis-dir $USER_DIR --method $METHOD
