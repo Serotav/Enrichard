d4530c6034756832e7ad42cfa2961f7a7020726a
Serotav <guliziasalvo@gmail.com>
2025-08-23 14:44:59 +0200

    fixing two sample 7: heatmaps again

--- app/App_Utils/pipeline.py
+++ app/App_Utils/pipeline.py
@@ -177,7 +177,8 @@ def process_and_merge_comparison_results(output_base_dir: pathlib.Path) -> pathl
     for file_a in result_files_a:
 
         if "_HEATMAP" in file_a:
-            fake_heatmap = comparison_dir / module_name / "_HEATMAP.tsv"
+            st.info("heatmap for {file_a}")
+            fake_heatmap = comparison_dir / module_name / f"{module_name}_HEATMAP.tsv"
             fake_heatmap.touch()
             continue
 
